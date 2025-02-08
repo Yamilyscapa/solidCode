106e66583e7f54f2b3d2a9573e3078b41519700c
Yamil Yscapa <yamilyscapa@MacBook-Pro-de-Yamil.local>
2025-02-07 18:05:40 -0600

    Fix: contact page typo

--- src/components/views/Contact.svelte
+++ src/components/views/Contact.svelte
@@ -6,7 +6,7 @@
 <div class="about grid-rows-auto container grid gap-y-12 text-center lg:grid-cols-2 lg:gap-x-12 lg:text-start">
 	<div class="about-text">
 		<h2 class="self-end font-heading text-5xl font-[500]">
-			Contactanos, estamos a un clic de distancia!
+			Contáctanos, estamos a un click de distancia!
 		</h2>
 		<p class="mt-6 self-start font-poppins text-gray-600">
 			¿Tienes preguntas? Estamos aquí para resolverlas. Envíanos un mensaje y nuestro equipo se
